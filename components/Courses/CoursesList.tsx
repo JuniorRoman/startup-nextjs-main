7c9f0540ec78ad751c3ad32947d46f726611fb9d
Junior Roman <goloborodko.rr@gmail.com>
2025-02-04 13:23:02 +0100

    tags

--- components/Courses/CoursesList.tsx
+++ components/Courses/CoursesList.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const CoursesList = ({ course }: { course: Course }) => {
-  const { title, image, paragraph, author, tags, publishDate } = course;
+  const { title, image, paragraph, author, publishDate } = course;
   return (
     <>
       <div
@@ -17,7 +17,7 @@ const CoursesList = ({ course }: { course: Course }) => {
           className="relative block aspect-[37/22] w-full"
         >
           <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white">
-            {tags}
+            {author.tags}
           </span>
           <Image src={image} alt="image" fill />
         </Link>
